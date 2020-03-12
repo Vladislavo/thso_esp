33d756e4f91c280f3a209b9c9bfac485fc36fd39
vladislav <vladislavrykov@hotmail.com>
2020-03-12 18:42:57 +0100

    sensors data recollection done

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,6 @@
 #define DHT22_READ_RETRIES                  100
 
 #define DHT22_PIN                           13
-#define I2C_SCL                             22
-#define I2C_SDA                             21
 
 typedef struct {
     float dht22_t   = .0;
@@ -29,6 +27,33 @@ typedef struct {
     float hih4030   = .0;
     float tmp102    = .0; // remove
     float hh10d     = .0;
+} esp_sensor_data_t;
+
+typedef struct {
+    float dht22_t   = .0;
+    float dht22_h   = .0;
+    float sht85_t   = .0;
+    float sht85_h   = .0;
+    float hih8121_t = .0;
+    float hih8121_h = .0;
+    float tmp102    = .0;
+    float hh10d     = .0;
+} wis_sensor_data_t;
+
+typedef struct {
+    float dht22_t   = .0;
+    float dht22_h   = .0;
+    float sht85_t   = .0;
+    float sht85_h   = .0;
+    float hih8121_t = .0;
+    float hih8121_h = .0;
+    float hh10d     = .0;
+} mkr_sensor_data_t;
+
+typedef struct {
+    esp_sensor_data_t esp_sensor_data;
+    wis_sensor_data_t wis_sensor_data;
+    mkr_sensor_data_t mkr_sensor_data;
 } sensor_data_t;
 
 DHT dht(DHT22_PIN, DHT22);
@@ -39,15 +64,15 @@ TMP102 tmp102(0x48);
 
 void setup_hh10d();
 
-void read_dht22(sensor_data_t *sensor_data);
-void read_sht85(sensor_data_t *sensor_data);
-void read_hih8121(sensor_data_t *sensor_data);
-void read_hih4040(sensor_data_t *sensor_data);
-void read_tmp36(sensor_data_t *sensor_data);
-void read_tmp102(sensor_data_t *sensor_data);
-void read_hh10d(sensor_data_t *sensor_data);
+void read_dht22(esp_sensor_data_t *sensor_data);
+void read_sht85(esp_sensor_data_t *sensor_data);
+void read_hih8121(esp_sensor_data_t *sensor_data);
+void read_hih4040(esp_sensor_data_t *sensor_data);
+void read_tmp36(esp_sensor_data_t *sensor_data);
+void read_tmp102(esp_sensor_data_t *sensor_data);
+void read_hh10d(esp_sensor_data_t *sensor_data);
 
-sensor_data_t sensor_data;
+esp_sensor_data_t sensor_data;
 
 void setup() {
     Serial.begin(BAUDRATE);
@@ -103,7 +128,7 @@ void setup_hh10d() {
 	  ofs  = i2cRead2bytes(HH10D_I2C_ADDRESS, 12);
 }
 
-void read_dht22(sensor_data_t *sensor_data) {
+void read_dht22(esp_sensor_data_t *sensor_data) {
     uint8_t retries = DHT22_READ_RETRIES;
 
     do {
@@ -118,18 +143,18 @@ void read_dht22(sensor_data_t *sensor_data) {
     } while ((isnan(isnan(sensor_data->dht22_t)) || isnan(sensor_data->dht22_h)) && retries);
 }
 
-void read_sht85(sensor_data_t *sensor_data) {
+void read_sht85(esp_sensor_data_t *sensor_data) {
     sht85.readSample();
     sensor_data->sht85_t = sht85.getTemperature();
     sensor_data->sht85_h = sht85.getHumidity();
 }
 
-void read_hih8121(sensor_data_t *sensor_data) {
+void read_hih8121(esp_sensor_data_t *sensor_data) {
     hih8121.read(&sensor_data->hih8121_t, &sensor_data->hih8121_h);
 }
 
 // read after the sht85
-void read_hih4040(sensor_data_t *sensor_data) {
+void read_hih4040(esp_sensor_data_t *sensor_data) {
     uint16_t adc_s = ads.readADC_SingleEnded(3);
     // magic numbers? -> read ads & hih4030 datasheets
     sensor_data->hih4030 = ((adc_s*0.1875)/1000)/0.031 - 25.8;
@@ -137,18 +162,18 @@ void read_hih4040(sensor_data_t *sensor_data) {
     sensor_data->hih4030 = sensor_data->hih4030 / (1.0546 - 0.0026 * sensor_data->sht85_t);
 }
 
-void read_tmp36(sensor_data_t *sensor_data) {
+void read_tmp36(esp_sensor_data_t *sensor_data) {
     sensor_data->tmp36_0 = (((ads.readADC_SingleEnded(0)*0.1875)/1000) - 0.5)*100;
     sensor_data->tmp36_1 = (((ads.readADC_SingleEnded(1)*0.1875)/1000) - 0.5)*100;
     sensor_data->tmp36_2 = (((ads.readADC_SingleEnded(2)*0.1875)/1000) - 0.5)*100;
 }
 
-void read_tmp102(sensor_data_t *sensor_data) {
+void read_tmp102(esp_sensor_data_t *sensor_data) {
     tmp102.wakeup();
     sensor_data->tmp102 = tmp102.readTempC();
 }
 
-void read_hh10d(sensor_data_t *sensor_data) {
+void read_hh10d(esp_sensor_data_t *sensor_data) {
     const int HH10D_FOUT_PIN    = 23;
     float freq = .0;
       for (int j=0; j < 256; j++) {
